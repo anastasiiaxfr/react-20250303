0c0ff855b00d0a2ecce8889982ce92fb39323a28
Zubkov Andrey <zubkov.andrey.99@mail.ru>
2025-03-10 19:13:18 +0300

    hw-2

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,25 @@
-import { createElement } from "react";
 import { createRoot } from "react-dom/client";
+import { headphones } from "./constants/mock";
 
 const root = document.getElementById("root");
 
 const reactRoot = createRoot(root);
 
-console.log(reactRoot);
-
-const DATA = [1, 2, 3, 4, 5];
-
-// reactRoot.render(
-//   createElement(
-//     "ul",
-//     { className: "someClass", style: { color: "red" } },
-//     createElement("li", {}, "1"),
-//     createElement("li", {}, "2"),
-//     createElement("li", {}, "3"),
-//     createElement("li", {}, "4"),
-//     createElement("li", {}, "5"),
-//     createElement("li", {}, "6"),
-//     createElement("li", {}, "7")
-//   )
-// );
-
 reactRoot.render(
-  <ul className='someClass' style={{ color: "red" }}>
-    {DATA.map((data) => (
-      <li>
-        <span>{data}</span>
-      </li>
+  <div>
+    <h1>Headphones</h1>
+    {headphones.map(({ name, id, reviews, codecs }) => (
+      <div key={id}>
+        <h2>{name}</h2>
+        <h3>Reviews</h3>
+        {reviews.map((text) => (
+          <span key={text}>{text}</span>
+        ))}
+        <h3>Codecs</h3>
+        {codecs.map((codec) => (
+          <span key={codec}>{codec}</span>
+        ))}
+      </div>
     ))}
-  </ul>
+  </div>
 );
